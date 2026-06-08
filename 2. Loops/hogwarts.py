e58ec47ae7a3a5adf92e62a4418f15e048d62589
SHAMIT MOOLYA <kingship176@gmail.com>
2026-06-09 01:04:11 +0530

    Updated 2.Loops

--- 2. Loops/hogwarts.py
+++ 2. Loops/hogwarts.py
@@ -83,4 +83,3 @@ students = [
 
 for student in students:
     print(student["name"], student["house"], student["patronus"], sep=", ")
-
---
+++ 2. Loops/shorts_sokoban06.py
@@ -0,0 +1,19 @@
+# LIST METHODS
+
+def main():
+    history = []
+
+    while True:
+        action = input("Action: ")
+
+        if action == "Undo":
+            undone = history.pop()
+            print(f"Undone: {undone}")
+        elif action == "Restart":
+            history.clear()
+        else:
+            history.append(action)
+        
+        print(history)
+
+main()
